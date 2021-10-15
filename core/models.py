ed41b865bd96ab39b146b103d4794880bcbf9648
nuraaly345 <kubanura57@gmail.com>
2021-10-15 20:27:17 +0300

    all

--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        to = User, on_delete=models.CASCADE,
+        related_name='profile'
+    )
+
+    region = models.CharField(max_length=1, choices = (
+        ('B','Bishkek'),
+        ('C', 'Chuy'),
+        ('N', 'Naryn'),
+        ('I', 'Issyk-kol'),
+        ('T', 'Talas'),
+        ('J', 'Jalal-abad'),
+        ('D', 'Batken'),
+    ))
+
+    photo = models.ImageField(
+        upload_to = 'profile',
+        null = True, blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
